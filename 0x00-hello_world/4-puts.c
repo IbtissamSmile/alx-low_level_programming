2657a61faa9b453c38c40b7129984bbff2c18fa8
root <btissamtoujni@gmail.com>
2023-02-16 13:59:50 -0800

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Print string in console using puts func.
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Print string in console using puts func.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
