f272e6890498740622cd4177d9fdfacd0f957b09
root <btissamtoujni@gmail.com>
2023-03-01 13:40:09 -0800

    8. rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -16,9 +16,9 @@ char *rot13(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == a[j])
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			for (j = 0; a[j] != '\0'; j++)
+			if (s[i] == a[j])
 			{
 				s[i] = b[j];
 				break;
