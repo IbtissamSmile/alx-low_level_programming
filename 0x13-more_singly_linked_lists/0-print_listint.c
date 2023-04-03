a2468461c416aae7782ab4a864aa298221a5a432
IbtissamSmile <btissamtoujni@gmail.com>
2023-04-03 16:52:24 -0700

    0. Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: the head of the list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	print(node->next;
+	node = node->next;
+	}
+	   while(node != NULL)
+}
