c4a94b61485c0b249ed24ebaf5a54c605e94aad6
IbtissamSmile <btissamtoujni@gmail.com>
2023-04-04 02:59:19 -0700

    14. Find the loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - the Funcion dada
+ * @head: Desde main
+ * Return: the loop starts
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *new_node1 = head, *new_node2 = head;
+
+	while (new_node1 && new_node2 && new_node2->next)
+	{
+		new_node1 = new_node1->next;
+		new_node2 = new_node2->next->next;
+		if (new_node1 == new_node2)
+		{
+			new_node1 = head;
+			while (new_node1 !=new_node2)
+			{
+				new_node1 = new_node1->next;
+				new_node2 = new_node2->next;
+			}
+			return (new_node1);
+		}
+	}
+	return (NULL);
+}
