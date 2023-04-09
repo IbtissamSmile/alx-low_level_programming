e4463d2e1dec69dffde84be3831dc88bceeda725
IbtissamSmile <btissamtoujni@gmail.com>
2023-04-09 16:13:26 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,7 +8,7 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t nop, nrd, nwr;
+	ssize_t o, r, w;
 	char *buf;
 
 	if (filename == NULL)
@@ -18,18 +18,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buf == NULL)
 		return (0);
 
-	nop = open(filename, O_RDONLY);
-	nrd = read(nop, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
+	o = open(filename, O_RDONLY);
+	r = read(o, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
 
-	if (nop == -1 || nrd == -1 || nwr == -1 || nwr != r)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buf);
 		return (0);
 	}
 
 	free(buf);
-	close(nop);
+	close(o);
 
-	return (nwr);
+	return (w);
 }
