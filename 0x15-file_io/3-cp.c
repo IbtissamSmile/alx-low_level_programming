3a2695f8c7789dddccb75640183e6e6f5c9c7485
IbtissamSmile <btissamtoujni@gmail.com>
2023-04-10 18:32:54 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
- * Return: A pointer to the newly-allocated buffer.
+ * main - copies the content of one file to another.
+ * @file: arguments count.
+ * Return: 1 on success, exit otherwise.
  */
 int main(int argc, char *argv[])
 {
-	int file_from, fto, rd, clf, clt;
+	int ffrom, fto, rd, clf, clt;
 	char buff[BUFSIZ];
 
 	if (argc != 3)
@@ -15,15 +15,15 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
+	ffrom = open(argv[1], O_RDONLY);
+	if (ffrom == -1)
 	{
 		dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	while ((rd = read(file_from, buff, BUFSIZ)) > 0)
+	while ((rd = read(ffrom, buff, BUFSIZ)) > 0)
 		if (fto == -1 || (write(fto, buff, rd) != rd))
 		{
 			dprintf(STDERR_FILENO,
@@ -36,13 +36,13 @@ int main(int argc, char *argv[])
 				"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	clf = close(file_from);
+	clf = close(ffrom);
 	clt = close(fto);
 	if (clf == -1 || clt == -1)
 	{
 		if (clf == -1)
 			dprintf(STDERR_FILENO,
-					"Error: Can't close fd %d\n", file_from);
+					"Error: Can't close fd %d\n", ffrom);
 		else if (clt == -1)
 			dprintf(STDERR_FILENO,
 					"Error: Can't close fd %d\n", fto);
