718118518bc50a0c2494fbe07ca7416d9e5f68e0
IbtissamSmile <btissamtoujni@gmail.com>
2023-04-10 18:21:05 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int ffrom, fto, nrd, clf, clt;
+	int f_from, fto, rd, clf, clt;
 	char buff[BUFSIZ];
 
 	if (argc != 3)
@@ -15,34 +15,34 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	ffrom = open(argv[1], O_RDONLY);
-	if (ffrom == -1)
+	f_from = open(argv[1], O_RDONLY);
+	if (f_from == -1)
 	{
 		dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	while ((nrd = read(ffrom, buff, BUFSIZ)) > 0)
-		if (fto == -1 || (write(fto, buff, nrd) != nrd))
+	while ((rd = read(f_from, buff, BUFSIZ)) > 0)
+		if (fto == -1 || (write(fto, buff, rd) != rd))
 		{
 			dprintf(STDERR_FILENO,
 					"Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-	if (nrd == -1)
+	if (rd == -1)
 	{
 		dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	clf = close(ffrom);
+	clf = close(f_from);
 	clt = close(fto);
 	if (clf == -1 || clt == -1)
 	{
 		if (clf == -1)
 			dprintf(STDERR_FILENO,
-					"Error: Can't close fd %d\n", ffrom);
+					"Error: Can't close fd %d\n", f_from);
 		else if (clt == -1)
 			dprintf(STDERR_FILENO,
 					"Error: Can't close fd %d\n", fto);
