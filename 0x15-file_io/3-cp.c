b3b1da868db7860e9d2e834206bdcd0639214ac8
IbtissamSmile <btissamtoujni@gmail.com>
2023-04-10 18:26:21 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int f_from, fto, rd, clf, clt;
+	int file_from, fto, rd, clf, clt;
 	char buff[BUFSIZ];
 
 	if (argc != 3)
@@ -15,15 +15,15 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	f_from = open(argv[1], O_RDONLY);
-	if (f_from == -1)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	while ((rd = read(f_from, buff, BUFSIZ)) > 0)
+	while ((rd = read(file_from, buff, BUFSIZ)) > 0)
 		if (fto == -1 || (write(fto, buff, rd) != rd))
 		{
 			dprintf(STDERR_FILENO,
@@ -36,13 +36,13 @@ int main(int argc, char *argv[])
 				"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	clf = close(f_from);
+	clf = close(file_from);
 	clt = close(fto);
 	if (clf == -1 || clt == -1)
 	{
 		if (clf == -1)
 			dprintf(STDERR_FILENO,
-					"Error: Can't close fd %d\n", f_from);
+					"Error: Can't close fd %d\n", file_from);
 		else if (clt == -1)
 			dprintf(STDERR_FILENO,
 					"Error: Can't close fd %d\n", fto);
