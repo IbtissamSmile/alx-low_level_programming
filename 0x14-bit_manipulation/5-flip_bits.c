2588d21b5177bc0f457c40e27534a0cd885f5464
root <btissamtoujni@gmail.com>
2023-05-04 12:54:58 -0700

    Write a function that returns the number

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,10 +8,12 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int diff = n ^ m, count = 0;
+
 	while (diff > 0)
 	{
 		count += (diff & 1);
 		diff >>= 1;
 	}
+
 	return (count);
 }
