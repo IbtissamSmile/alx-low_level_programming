47cafc469ba92202a71d4b55d5a8bf51d1a8440a
root <btissamtoujni@gmail.com>
2023-05-08 11:52:12 -0700

    update main.c

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * main - check the code
  * Return: Always 0.
@@ -8,6 +9,7 @@
 int main(int ac, char **av)
 {
 	ssize_t n;
+
 	if (ac != 2)
 	{
 		dprintf(2, "Usage: %s filename\n", av[0]);
