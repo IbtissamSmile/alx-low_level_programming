bd7ea41949fff04879001605f5ca9c907789961a
root <btissamtoujni@gmail.com>
2023-05-08 11:57:19 -0700

    . Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * create_file - creates a file
  * @filename: the filename.
@@ -8,17 +9,23 @@
 int create_file(const char *filename, char *text_content)
 {
 	int nop, nwr, len = 0;
+
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
+
 	nop = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	nwr = write(nop, text_content, len);
+
 	if (nop == -1 || nwr == -1)
 		return (-1);
+
 	close(nop);
+
 	return (1);
 }
