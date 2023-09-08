60f3ee15258561f48a3b0974bf47f5618e2de4a3
IbtissamSmile <btissamtoujni@gmai.com>
2023-09-08 13:44:57 -0700

    PHP Internals Book: HashTable

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -138,7 +138,7 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, ht->size);
 	if (index >= ht->size)
 		return (NULL);
-	
+
 	node = ht->shead;
 	while (node != NULL && strcmp(node->key, key) != 0)
 		node = node->snext;
